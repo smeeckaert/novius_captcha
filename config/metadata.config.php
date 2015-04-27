376338a8f01783298933534f724c6f32daa2eef6
Martin Smeeckaert <smeeckaert@novius.fr>
2015-04-27 16:13:11 +0200

    first commit

---
+++ config/metadata.config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * NOVIUS OS - Web OS for digital communication
+ *
+ * @copyright  2013 Novius
+ * @license    GNU Affero General Public License v3 or (at your option) any later version
+ *             http://www.gnu.org/licenses/agpl-3.0.html
+ * @link       http://www.novius-os.org
+ */
+
+return array(
+    'name'      => __('Novius Captcha'),
+    'version'   => '1.0',
+    'provider'  => array(
+        'name' => 'Smeeckaert Martin',
+    ),
+    'namespace' => 'Novius\\Captcha',
+    'icons'     => array(
+        64 => 'static/apps/novius_captcha/img/icon/64.png',
+        32 => 'static/apps/novius_captcha/img/icon/32.png',
+        16 => 'static/apps/novius_captcha/img/icon/16.png',
+    ),
+    'launchers' => array(),
+    'enhancers' => array()
+);
