caff666357f7f9083eaeeb29d5bb244c4ad543e4
Martin Smeeckaert <smeeckaert@novius.fr>
2015-04-27 17:25:41 +0200

    label

--- views/front/Driver/SimpleMath.view.php
+++ views/front/Driver/SimpleMath.view.php
@@ -4,12 +4,12 @@ $config = \Config::load('novius_captcha::driver/simplemath');
 
 ?>
 <div class="captcha simple-math">
-    <span>
-        <?= \Arr::get($config, 'question.label', '') ?>
+    <label for=<?= \Arr::get($params, 'id') ?>><?= \Arr::get($config, 'question.label', '') ?>
         <?= \Arr::get($config, 'question.bold-equation', false) ? '<strong>' : '' ?>
         <?= implode(' ', array($one, $sign, $two)) ?>
         <?= \Arr::get($config, 'question.bold-equation', false) ? '</strong>' : '' ?>
-    </span>
+    </label>
+
     <input type="text" name="<?= $name ?>" <?=
     $output = implode(' ', array_map(function ($v, $k) {
         return $k . '="' . $v . '"';
